5b464252d0dd89a2906852aa91bd3924da594e08
Maxim Isaev <isaevmaxim2010@gmail.com>
2022-05-18 20:50:41 +0300

    add: begin with header

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <header class="header">
+    <div class="header__logo">
+      <h1><a href="#">mangcoding Store</a></h1>
+    </div>
+    <!-- <nav class="header__nav">
+      <ul class="header__link">
+        <li><a href="">Home</a></li>
+        <li><a href="">Shop</a></li>
+        <li><a href="">About Us</a></li>
+        <li><a href="">Contact</a></li>
+      </ul>
+    </nav>
+    <div class="header__icons">
+      <img src="" alt="">
+      <img src="" alt="">
+      <img src="" alt="">
+    </div>
+    <div class="header__input">
+
+    </div> -->
+  </header>
+  
+  <!-- <footer class="footer">
+    <h1>Footer</h1>
+  </footer> -->
+</template>
+
+<script>
+</script>
+
+<style lang="scss">
+@import './css/variables.scss';
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="Home">
+    <h1 class="header">Header</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.header {
+  color: red
+}
+</style>
+
